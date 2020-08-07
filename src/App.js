1f86d66f1f46ad04d02803193a2afd8256f8dfde
Joseph M <jmedina1985@hotmail.com>
2020-08-06 21:17:54 -0700

    Pre eject and headline component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Affirmation Generator</h1>
-    </div>
-  );
+import Headline from "./Components/Headline/Headline";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Headline />
+        <div>Information</div>
+        <div>Affirmation</div>
+        <button>Generate</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Headline/Headline.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Headline.css";
+
+const headline = () => {
+  return <h1 className={classes.headline}>Affirmation Generator</h1>;
+};
+
+export default headline;
