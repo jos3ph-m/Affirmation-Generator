56c18158fb7b2b046b0db9deeeca44206f967a16
Joseph M <jmedina1985@hotmail.com>
2020-08-08 11:40:46 -0700

    Resolved affirmations issue

--- src/Components/Affirmations/Affirmations.js
+++ src/Components/Affirmations/Affirmations.js
@@ -3,34 +3,36 @@ import React, { Component } from "react";
 // import Affirmation from "./Affirmation/Affirmation";
 // import Button from "./Button/Button";
 
-const affirmations = () => {
-  const affirmationsArray = [
-    "You are a lovable, wonderful friend.",
-    "You are super talented! Look at you go!",
-    "Someone in your life looks up to you, you're amazing!",
-    "You deserve nothing but the best!",
-    "You're doing a great job.",
-    "You're here because you matter.",
-    "You are beautiful inside and out.",
-    "Here's a reminder: love yourself first.",
-  ];
-
-  let codex = "Click the button below to change the affirmation given";
-
-  const shuffle = () => {
-    let index = Math.floor(
-      Math.random() * Math.floor(affirmationsArray.length)
-    );
-
-    return (codex = affirmationsArray[index]);
+class Affirmations extends Component {
+  state = {
+    message: "Click generate below",
   };
 
-  return (
-    <div>
-      <p></p>
-      <button onClick={shuffle}>Generate</button>
-    </div>
-  );
-};
+  shuffle = () => {
+    let affirmations = [
+      "You are a lovable, wonderful friend.",
+      "You are super talented! Look at you go!",
+      "Someone in your life looks up to you, you're amazing!",
+      "You deserve nothing but the best!",
+      "You're doing a great job.",
+      "You're here because you matter.",
+      "You are beautiful inside and out.",
+      "Here's a reminder: love yourself first.",
+    ];
 
-export default affirmations;
+    let index = Math.floor(Math.random() * Math.floor(affirmations.length));
+    let newMessage = affirmations[index];
+    this.setState({ message: newMessage });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.message}</p>
+        <button onClick={this.shuffle}>Generate</button>
+      </div>
+    );
+  }
+}
+
+export default Affirmations;
