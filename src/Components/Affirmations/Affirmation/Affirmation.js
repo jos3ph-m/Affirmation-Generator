04d06675507c83c979f15e48287c0260801b972a
Joseph M <jmedina1985@hotmail.com>
2020-08-09 18:17:24 -0700

    Cleaned folder structure

--- src/Components/Affirmations/Affirmation/Affirmation.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const affirmation = () => {
-  return <div>I am an affirmation</div>;
-};
-
-export default affirmation;
--- src/Components/Affirmations/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const button = () => {
-  return <button>And I am its button</button>;
-};
-
-export default button;
