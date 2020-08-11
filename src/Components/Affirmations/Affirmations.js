85dfa861bef44e6a43c993174a71519f3e3b2d32
Joseph M <jmedina1985@hotmail.com>
2020-08-10 19:27:04 -0700

    Finalizing margins and spacing

--- src/Components/Affirmations/Affirmations.js
+++ src/Components/Affirmations/Affirmations.js
@@ -4,7 +4,7 @@ import styles from "./Affirmations.module.css";
 
 class Affirmations extends Component {
   state = {
-    message: "Click generate below",
+    message: "",
   };
 
   shuffle = () => {
